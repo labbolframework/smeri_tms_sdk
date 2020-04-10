65ca35df3bf51bf1978e0dde86eea7b8a2d99c5d
YL <yl1430834495@163.com>
2020-04-10 15:36:48 +0800

    发布1.0.16版本

--- src/main/java/com/labbol/smeri/api/test/report/request/CreateReportRequest.java
+++ src/main/java/com/labbol/smeri/api/test/report/request/CreateReportRequest.java
@@ -17,7 +17,7 @@ public class CreateReportRequest extends AbstractAPIRequest<CreateReportResponse
 	private Report report;	
 	
 	private static final String [] REPORT_NOT_BLANK_FIELDS = {"poaId","tokenId","reportNo","reportName",
-			"testEndDate","testAddr","tester","checker","conclusion","groupId",
+			"testDate","testAddr","tester","checker","conclusion","groupId",
 			"fileUrl","reportGenSate","creator","updator","state"};
 	
 	public CreateReportRequest() {
